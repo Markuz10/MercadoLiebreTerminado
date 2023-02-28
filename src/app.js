9b78438846129e6dd6f84f10ceb0cef56b3b8529
Markuz10 <marko.dv@hotmail.com>
2023-02-27 22:03:27 -0300

    4rto cambio

--- src/app.js
+++ src/app.js
@@ -3,22 +3,23 @@ const app = express();
 const path = require("path");
 
 
-app.set("vierws",path.resolve(__dirname,"./vierws"));
-app.use(express.static(path.resolve(__dirname,"../public")));
+app.use(express.static("public"));
 
-app.listen(port,console.log("Servidor ok"));
+const port = process.env.PORT || 3020;
 
-const port = process.env.PORT ||3020;
+app.listen(port,() => 
+console.log ("el Servidor ta on" + port)
+);
 
-// ruta
-app.get("/",(req,res)=>{
-    res.sendfile(path.join(__dirname,"./vierws/index.html"));
+app.get("/",(req,res) => {
+    res.sendFile(path.resolve(__dirname,"./vierws/home.html"));
+})
+
+app.get("/",(req,res) => {
+    res.sendFile(path.resolve(__dirname,"./vierws/register.html"));
 })
 
 
-app.get("/",(req,res)=>{
-    res.sendfile(path.join(__dirname,"./vierws/register.html"));
-})
-app.get("/",(req,res)=>{
-    res.sendfile(path.join(__dirname,"./vierws/login.html"));
+app.get("/",(req,res) => {
+    res.sendFile(path.resolve(__dirname,"./vierws/login.html"));
 })
